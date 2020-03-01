33a46b61838ddbcc005c087998b9205ecc01c304
unknown <abdelhedi.hlel@oyez.fr>
2020-03-01 10:23:10 +0100

    project init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import HeaderWrapper from './HeaderWrapper'
+
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <div className="header__container">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <ul>
+            <li>Our Team</li>
+            <li>Training</li>
+            <li>Nutrition</li>
+            <li>Nutrition</li>
+            <li>Store</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+      </div>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/HeaderWrapper.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: inline-flex; */
+  width: 100%;
+  background-color: red;
+`;
+export default Wrapper;
